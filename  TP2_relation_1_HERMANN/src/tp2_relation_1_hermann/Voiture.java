b8f4035720145582918b478e7f3331e608734c1f
PaulHERMANN <paul.hermann@epfedu.fr>
2025-11-12 09:36:18 +0100

    tp2 4

---
+++  TP2_relation_1_HERMANN/src/tp2_relation_1_hermann/Voiture.java
@@ -0,0 +1,28 @@
+package tp2_relation_1_hermann;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author paulh
+ */
+public class Voiture {
+    String Modele;
+    String Marque;
+    int PuissanceCV;
+
+    public Voiture(String Modele, String Marque, int PuissanceCV) {
+        this.Modele = Modele;
+        this.Marque = Marque;
+        this.PuissanceCV = PuissanceCV;
+    }
+
+    @Override
+    public String toString() {
+        return "Voiture{" + "Modele=" + Modele + ", Marque=" + Marque + ", PuissanceCV=" + PuissanceCV + '}';
+    }
+   
+}
