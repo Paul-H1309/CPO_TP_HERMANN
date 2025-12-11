845355d407728b592aa0a6b038c32ae5afc6b93a
PaulHERMANN <paul.hermann@epfedu.fr>
2025-12-11 15:33:38 +0100

    v4

---
+++  Cadenas/src/Resultat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author paulh
+ */
+public class Resultat {
+    
+    int justes;
+    int tropHauts;
+    int tropBas;
+    
+}
