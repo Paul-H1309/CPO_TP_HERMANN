dac605072b8d1923aa9b8638f255d6dc31c284eb
PaulHERMANN <paul.hermann@epfedu.fr>
2025-12-13 19:36:03 +0100

    avancé sur les class

---  Cadenas/src/Resultat.java
+++  Cadenas/src/Resultat.java
@@ -12,5 +12,9 @@ public class Resultat {
     int justes;
     int tropHauts;
     int tropBas;
+
+    void analyser(int[] Tab0, int[] TabC) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
